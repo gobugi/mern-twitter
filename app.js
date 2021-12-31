8454a9bc24d6086aec137f96dcaf8541872e6a7d
johnny <johnpark1987@gmail.com>
2021-12-31 15:50:06 -0800

    register

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const User = require("./models/User");
-
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to mongoDB"))
   .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   const user = new User({
@@ -23,10 +25,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
